80f84b6966a60e4d310d70d4355ff923ca62bc33
matias zazzali <matizazzali@gmail.com>
2021-10-24 12:01:24 +0200

    edit sql queries

--- SQL/queriesForTableau.sql
+++ SQL/queriesForTableau.sql
@@ -1,7 +1,7 @@
 -- Covid 19 project. Queries to use when loading data in Tableau
 
 -- Complete table of interest without the continents/world as locations
-select dea.continent, dea.location, dea.date, dea.population, total_cases, cast(total_deaths as int) as total_deaths,(total_deaths)/population*100 as death_rate, cast(people_vaccinated as float) as people_vaccinated, cast(people_fully_vaccinated as float) as people_fully_vaccinated, (people_vaccinated/dea.population)*100 as percentage_people_vaccinated, (people_fully_vaccinated/dea.population)*100 as percentage_people_fully_vaccinated
+select dea.continent, dea.location, dea.date, dea.population, total_cases, cast(total_deaths as int) as total_deaths, new_cases, cast(new_deaths as int) as new_deaths,(cast(total_deaths as int)/total_cases)*100 as death_rate, cast(new_vaccinations as int) as new_vaccinations,cast(people_vaccinated as float) as people_vaccinated, cast(people_fully_vaccinated as float) as people_fully_vaccinated, (people_vaccinated/dea.population)*100 as percentage_people_vaccinated, (people_fully_vaccinated/dea.population)*100 as percentage_people_fully_vaccinated
 from CovidProject..CovidVaccinations as vac
 	join CovidProject..CovidDeaths as dea
 	on vac.date = dea.date
@@ -10,7 +10,7 @@ where dea.continent is not null
 order by dea.location,dea.date
 
 -- Complete table of interest globally
-select dea.date, dea.population, total_cases, cast(total_deaths as int) as total_deaths, (total_deaths)/population*100 as death_rate, cast(people_vaccinated as float) as people_vaccinated, cast(people_fully_vaccinated as float) as people_fully_vaccinated, (people_vaccinated/dea.population)*100 as percentage_people_vaccinated, (people_fully_vaccinated/dea.population)*100 as percentage_people_fully_vaccinated
+select dea.date, dea.population, total_cases, new_cases, cast(new_deaths as int) as new_deaths, cast(total_deaths as int) as total_deaths, (cast(total_deaths as int)/total_cases)*100 as death_rate, cast(people_vaccinated as float) as people_vaccinated, cast(new_vaccinations as int) as new_vaccinations,cast(people_fully_vaccinated as float) as people_fully_vaccinated, (people_vaccinated/dea.population)*100 as percentage_people_vaccinated, (people_fully_vaccinated/dea.population)*100 as percentage_people_fully_vaccinated
 from CovidProject..CovidVaccinations as vac
 	join CovidProject..CovidDeaths as dea
 	on vac.date = dea.date
